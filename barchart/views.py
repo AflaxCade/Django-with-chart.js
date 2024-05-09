c088e0036c7acc32a7c518bfe0a15701001b6e5a
AflaxCade <aflah.cade@gmail.com>
2024-05-09 17:24:50 +0300

    Added forms.py and implemented adding product in the view

--- barchart/views.py
+++ barchart/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 # Create your views here.
 
 def index(request):
     data = Product.objects.all()
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ProductForm()
     context = {
         'data': data
     }
